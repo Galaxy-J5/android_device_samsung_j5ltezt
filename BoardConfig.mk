ef3702860f1f2ab6bf7772bb9e25594d4c881e51
Ganesh Varma <ganeshvarmai4u@gmail.com>
2016-12-24 20:45:48 +0530

    moved flags to common

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -48,10 +48,6 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j5ltezt
 
-# Kernel
-TARGET_KERNEL_CONFIG := cyanogenmod_j5nlte_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/j5lte
-
 # Vendor Init
 TARGET_UNIFIED_DEVICE := true
 TARGET_INIT_VENDOR_LIB := libinit_msm
